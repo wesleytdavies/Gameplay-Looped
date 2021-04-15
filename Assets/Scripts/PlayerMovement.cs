f2ac1ad1c96c96b2d22999bc3d74fb0c6fe853b4
Wesley Davies <wtd224@nyu.edu>
2021-04-15 13:09:02 -0400

    added player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+    private bool isMoving = false;
+    private Vector2 moveDirection;
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        moveDirection = Vector2.zero;
+    }
+
+    void Update()
+    {
+        //directional movement
+        if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.D))
+        {
+            isMoving = true;
+            moveDirection.x = 1 / Mathf.Sqrt(2);
+            moveDirection.y = 1 / Mathf.Sqrt(2);
+        }
+        else if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.A))
+        {
+            isMoving = true;
+
+            moveDirection.x = -1 / Mathf.Sqrt(2);
+            moveDirection.y = 1 / Mathf.Sqrt(2);
+        }
+        else if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.D))
+        {
+            isMoving = true;
+
+            moveDirection.x = 1 / Mathf.Sqrt(2);
+            moveDirection.y = -1 / Mathf.Sqrt(2);
+        }
+        else if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.A))
+        {
+            isMoving = true;
+            moveDirection.x = -1 / Mathf.Sqrt(2);
+            moveDirection.y = -1 / Mathf.Sqrt(2);
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {
+            isMoving = true;
+            moveDirection.x = 0;
+            moveDirection.y = 1;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            isMoving = true;
+            moveDirection.x = 0;
+            moveDirection.y = -1;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            isMoving = true;
+            moveDirection.x = 1;
+            moveDirection.y = 0;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            isMoving = true;
+            moveDirection.x = -1;
+            moveDirection.y = 0;
+        }
+        else
+        {
+            isMoving = false;
+            rb.velocity = new Vector2(0, 0);
+        }
+        if (isMoving)
+        {
+            rb.velocity = new Vector2(moveSpeed * moveDirection.x, moveSpeed * moveDirection.y);
+        }
+    }
+}
